a9e3e422b9236533e50a8a3a4ec9ee4004bfe835
julieeen <julieeen@web.de>
2014-06-19 11:35:55 +0200

    Update gc_project2.cpp

--- gc_project2.cpp
+++ gc_project2.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : OpenGLAufgabe.cpp
-// Author      : 
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
